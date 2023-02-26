a0eca2bb683f2efa1e4866908e50740b3ca4ee1c
Cauã Yves <cauayves5@gmail.com>
2023-02-26 15:52:07 -0300

    teclado desabilitado ao ganhar ou perder o jogo

--- src/components/Letras.jsx
+++ src/components/Letras.jsx
@@ -1,20 +1,98 @@
+// import alfabeto from '../alfabeto'
+// export default function Letras({ disableLetter, arrayLetter, palavra, word, setWord, setAttempts, attempts, setOverClass, finalWord, setArrayLetter }) {
+//   let failsLetter
+//   function showWord() {
+//     setWord(finalWord)
+//   }
+//   console.log(finalWord)
+//   function disabledAlphabetBtns() {
+//     arrayLetter = alfabeto.map(i => i.toLowerCase())
+//     setArrayLetter(arrayLetter)
+//     setOverClass('gameOverLose')
+//   }
+//   function gameOver{
+//     showWord()
+//     disabledAlphabetBtns()
+//     changeImg()
+//   }
+//   function changeImg() {
+//     setAttempts((attempts + 1))
+//   }
+
+//   function checkLetter(letter) {
+//     let newWord = palavra.split("").map((i, index) => {
+//       if (letter.toLowerCase() === i) {
+//         return i
+//       } else {
+//         failsLetter++
+//         if (failsLetter === palavra.length && attempts < 5) {
+//           changeImg()
+//         } else if (failsLetter === palavra.length && attempts === 5) {
+//           gameOver()
+//         }
+//         return word[index]
+//       }
+//     })
+    
+//     // Verifica se a palavra foi completa
+//     if (!newWord.includes('_')) {
+//       setOverClass('gameOverWin')
+//     }
+  
+//     if (attempts >= 5) {
+//       gameOver()
+//     } else {
+//       setWord(newWord)
+//     }
+//   }
+  
+
+
+//   return (
+//     <div className="alfabeto-contain">
+//       {alfabeto.map(i => {
+//         return (
+//           <button
+//             key={i}
+//             className="alfabeto-letter"
+//             disabled={(arrayLetter.length === 0 || arrayLetter.includes(i)) ? true : false}
+//             onClick={() => {
+//               failsLetter = 0;
+//               disableLetter(i)
+//               checkLetter(i)
+//             }} data-test="letter">
+//             {i}
+//           </button>
+//         )
+//       })}
+//     </div>
+//   )
+// }
+
 import alfabeto from '../alfabeto'
+import { useState } from 'react'
+
 export default function Letras({ disableLetter, arrayLetter, palavra, word, setWord, setAttempts, attempts, setOverClass, finalWord, setArrayLetter }) {
+  const [gameOver, setGameOver] = useState(false)
   let failsLetter
-  function showWord() {
-    setWord(finalWord)
-  }
-  console.log(finalWord)
-  function disabledAlphabetBtns() {
-    arrayLetter = alfabeto.map(i => i.toLowerCase())
-    setArrayLetter(arrayLetter)
-    setOverClass('gameOverLose')
-  }
-  function gameOver() {
+  
+  function gameover() {
     showWord()
     disabledAlphabetBtns()
     changeImg()
   }
+
+  function showWord() {
+    console.log('a palavra era', finalWord)
+    setWord(finalWord)
+  }
+
+  function disabledAlphabetBtns() {
+    setGameOver(true)
+    setArrayLetter(alfabeto.map(i => i.toLowerCase()))
+    setOverClass('gameOverLose')
+  }
+
   function changeImg() {
     setAttempts((attempts + 1))
   }
@@ -28,7 +106,7 @@ export default function Letras({ disableLetter, arrayLetter, palavra, word, setW
         if (failsLetter === palavra.length && attempts < 5) {
           changeImg()
         } else if (failsLetter === palavra.length && attempts === 5) {
-          gameOver()
+          gameover()
         }
         return word[index]
       }
@@ -36,17 +114,16 @@ export default function Letras({ disableLetter, arrayLetter, palavra, word, setW
     
     // Verifica se a palavra foi completa
     if (!newWord.includes('_')) {
+      setGameOver(true)
       setOverClass('gameOverWin')
     }
-  
+
     if (attempts >= 5) {
-      gameOver()
+      gameover()
     } else {
       setWord(newWord)
     }
   }
-  
-
 
   return (
     <div className="alfabeto-contain">
@@ -55,7 +132,7 @@ export default function Letras({ disableLetter, arrayLetter, palavra, word, setW
           <button
             key={i}
             className="alfabeto-letter"
-            disabled={(arrayLetter.length === 0 || arrayLetter.includes(i)) ? true : false}
+            disabled={(arrayLetter.length === 0 || arrayLetter.includes(i) || gameOver) ? true : false}
             onClick={() => {
               failsLetter = 0;
               disableLetter(i)
@@ -68,6 +145,7 @@ export default function Letras({ disableLetter, arrayLetter, palavra, word, setW
     </div>
   )
 }
+
 
 
 
